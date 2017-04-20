1889797ef52d9068877905ed6e1ae90773647559
Avi Goldman <avrahamymgoldman@gmail.com>
2017-04-20 14:39:51 -0400

    added batch sending to postcards from to prop

--- index.js
+++ index.js
@@ -8,7 +8,41 @@ function lobPlus(lob) {
     throw new Error('lob must be an instance of Lob Node library');
   }
 
+  // batching from the params.to value if its an array
+  attachBatchSend(lob.postcards);
+
   return lob;
 };
+
+/**
+ * Overrides the create function to allow for batching from the "to" parameter in the params
+ * @param  {Object} resource  The lob resource to modify (postcards, letters, etc.)
+ */
+function attachBatchSend(resource) {
+  resource.pureCreate = resource.create; // save the original create for later
+
+  resource.create = function(params, callback) {
+    if (!(params.to instanceof Array)) {
+      return this.pureCreate.apply(this, arguments);
+    }
+
+    let recipients = params.to;
+    let payloads = [];
+
+    for (let i = 0; i < recipients.length; i++) {
+      let newParams = JSON.parse(JSON.stringify(params));
+      newParams.to = recipients[i];
+
+      merge(newParams, newParams.to.overrides || {});
+      delete newParams.to.overrides;
+
+      payloads.push(newParams);
+    }
+
+    return batchRequest(function(params, callback) {
+      return resource.pureCreate(params, callback);
+    }, payloads).asCallback(callback);
+  };
+}
 
 module.exports = lobPlus;
