80fe1f7b04ea1d49a33dcabb500658c67659e487
Avi Goldman <avrahamymgoldman@gmail.com>
2017-04-20 14:35:48 -0400

    wrote func to pull resources from lob object

---
+++ utils/getResources.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Returns an object of resources from the given lob object
+ * @param  {Lob}     lob       A lob instance
+ * @return {Object}  resources An object of resources
+ */
+function getResources(lob) {
+  const ResourceBase = lob.resourceBase;
+  let resources = {};
+
+  Object.keys(lob).forEach((key) => {
+    const resource = lob[key];
+    if (resource instanceof ResourceBase && resource !== ResourceBase) {
+      resources[key] = resource;
+    }
+  });
+
+  return resources;
+}
+
+module.exports = getResources;
