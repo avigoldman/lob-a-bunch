175b1e1b3f11a2992080a9cd015d6573f36fba16
Avi Goldman <avrahamymgoldman@gmail.com>
2017-04-26 13:32:56 -0400

    rewrote base of batchRequest to be a little clearer

--- utils/batchRequest.js
+++ utils/batchRequest.js
@@ -2,156 +2,149 @@
 
 const BBPromise = require('bluebird');
 
-/**
- * A requester - pulls from the queue and makes the requests recursively until the queue is empty
- * @param {Object}   params                  An object of required params with vars shared across requesters
- * @param {Function} params.func             The function to call recursively
- * @param {Array}    params.queue            An array of payloads to send through the func
- * @param {Object}   params.state            The internal state
- * @param {Object}   params.combinedResults  The object with the request results stored on it
- * @param {Function} params.done             The function called when the queue is empty by each requester
- */
-function requester(params) {
-  const { func, queue, state, combinedResults, done } = params;
-
-  // close this requester if there are no more items in the queue
-  if (queue.length <= 0) {
-    state.requester_count--;
-    return done();
+function BatchRequest(config, done) {
+  if (!(this instanceof BatchRequest)) {
+    return new BatchRequest(config, done);
   }
 
-  // wait the specified time from rate limitng
-  if (state.waiting) {
-    return setTimeout(() => {
-      return requester(params);
-    }, state.waiting);
+  if (typeof config.action !== 'function') {
+    return done(new Error('Action must be given to BatchRequest'));
   }
 
-  const payload = queue.shift();
+  if (config.max_requests < 1) {
+    return done(new Error('Max requests must be greater or equal to 1'));
+  }
 
-  func(payload, (err, result) => {
+  this.maxRequests = config.max_requests || 25;
+  this.queue = config.queue || [];
+  this.action = config.action;
+  this.every = config.every || function() {};
+  this.done = done || function() {};
+  
+  this.numberOfRequests = 0;  
+  this.paused = false;
+  this.pauseCount = 0;
+  this.lastPause = new Date().getTime();
+
+  this.results = {
+    errors: [],
+    data: []
+  };
+
+  this.triggerRequests();
+}
+
+BatchRequest.prototype.handleResponse = function(type, request, response) {
+  this.numberOfRequests--;
+
+  if (type === 'error') {
+    response._request = request;
+    this.results.errors.push(response);
+    this.every(response);
+  }
+
+  else if (type === 'success') {
+    this.results.data.push(response);
+    this.every(null, response);
+  }
+
+  else if (type === 'rateLimit') {
+    this.queue.unshift(request);
+
+    if (this.paused === false) {
+      const pauseTime = this.calculatePauseTime(response);
+      this.pause();
+
+      setTimeout(() => { this.play(); }, pauseTime);
+    }
+  }
+
+  if (this.isFinished()) {
+    return this.finish();
+  }
+  
+  this.triggerRequests();
+};
+
+BatchRequest.prototype.pause = function() {
+  this.pauseCount = this.paused ? this.pauseCount : this.pauseCount+1;
+  this.lastPause = new Date().getTime();
+  this.paused = true;
+};
+
+BatchRequest.prototype.play = function() {
+  this.paused = false;
+  this.triggerRequests();
+};
+
+BatchRequest.prototype.calculatePauseTime = function(response) {
+  const timeSinceStart = new Date().getTime() - this.lastPause;
+  const waitTime = (response._response.headers['rate-limit-window'] * 1000) - timeSinceStart;
+
+  return waitTime > 0 ? waitTime : 0;
+}
+
+BatchRequest.prototype.makeRequest = function(request, callback) {
+  this.numberOfRequests++;
+
+  this.action(request, (err, result) => {
     const response = err || result;
-    // we errored out and hit the rate limit
+    let type = !!err ? 'error' : 'success';
+
     if (hitRateLimit(response)) {
-      state.actually_hit++;
-      queue.push(payload);
-  
-      // if this is the first requester to hit the rate limit alert the others
-      if (!state.waiting) {
-        combinedResults.batches_count++;
-        
-        state.round_start_time = new Date().getTime(); // update the start time to this round
-        state.waiting = calculateWaitTime(response, state.round_start_time);
-
-        // this timeout will be the first requester to resume and will clear out the waiting var
-        return setTimeout(() => {
-          state.waiting = null;
-
-          return requester(params);
-        }, state.waiting);
-      }
-
-      return requester(params);
-    }
-    
-    // the request succeeded or came back with an error other than a rate limit
-    if (err) {
-      err.payload = payload;
-      combinedResults.errors.push(err);
-    }
-    else {
-      combinedResults.data.push(result);
+      type = 'rateLimit';
     }
 
-    combinedResults.rejected_count += !!err;
-    combinedResults.accepted_count += !!result;
-
-    state.every(err, result);
-
-    return requester(params);
+    callback(type, request, response);
   });
 }
 
-/**
- * Checks if the given result indicates we hit the rate limit
- * @param  {Object} result  The result/err from the request
- * @return {Boolean}        Boolean if we hit the rate limit
- */
-function hitRateLimit(result) {
-  return 'rate-limit-remaining' in result._response.headers && parseInt(result._response.headers['rate-limit-remaining']) === 0;
-}
+BatchRequest.prototype.triggerRequests = function() {
+  if (this.paused) return;
 
-/**
- * Uses the headers and time since we started a new round
- * @param  {Object} result         result the result/err from the request
- * @param  {Int} roundStartTime    start time in milliseconds
- * @return {Int}                   Milliseconds to pause
- */
-function calculateWaitTime(result, roundStartTime) {
-  const now = new Date().getTime();
-  const timeSinceStart = now - roundStartTime;
-  const waitTime = (result._response.headers['rate-limit-window'] * 1000) - timeSinceStart;
+  while(this.numberOfRequests < this.maxRequests && this.queue.length > 0) {
+    this.makeRequest(this.queue.shift(), (type, request, response) => {
+      this.handleResponse(type, request, response);
+    });
+  }
+};
 
-  if (waitTime > 0) {
-    return waitTime;
+BatchRequest.prototype.isFinished = function() {
+  return this.numberOfRequests === 0 && this.queue.length === 0;
+};
+
+
+BatchRequest.prototype.finish = function() {
+  const results = { 
+    rejected_count: this.results.errors.length,
+    accepted_count: this.results.data.length,
+    batch_count: this.pauseCount,
+    errors: this.results.errors,
+    data: this.results.data
+  };
+
+  if (results.rejected_count === 0) {
+    delete results.errors;
   }
 
-  return 0;
-}
-
+  if (results.rejected_count > 0 && results.accepted_count === 0) {
+    const error = new Error('Failed to make batch request.');
+    error.results = results;
+    this.done(error);
+  }
+  else {
+    this.done(null, results);
+  }
+};
 
 /**
- * Takes a function, queue, and settings and makes requests
- * @param  {Function} func      The function to call for each value in the queue
- * @param  {Array}    queue     An array of payloads to send through the func
- * @param  {Object}   settings  Settings for the requests
- * @return {Promise}            A promise that resolves when the queue is empty and all requests have finished
+ * Checks if the given response indicates we hit the rate limit
+ * @param  {Object} response  The result/err from the request
+ * @return {Boolean}          Boolean if we hit the rate limit
  */
-module.exports = function batchRequest(func, queue, settings = {}) {
-  return new BBPromise((resolve, reject) => {
-    const state = {
-      round_start_time: new Date().getTime(),
-      max_requests: settings.max_requests || 25,
-      every: settings.every || function(err, result) {},
-      requester_count: 0,
-      waiting: null,
-      actually_hit: 0
-    }; 
-    
-    // max_requests should never be more then the items in the queue
-    state.max_requests = state.max_requests > queue.length ? queue.length : state.max_requests;
-
-    const combinedResults = {
-      rejected_count: 0,
-      accepted_count: 0,
-      batches_count: 0,
-      errors: [],
-      data: []
-    };
-
-    const done = () => {
-      if (state.requester_count > 0) { return; }
-
-      // some 5xx and no accepted results means failure      
-      if (combinedResults.errors.length > 0 && combinedResults.accepted_count === 0) {
-        const e = new Error('Failed to make batch request.');
-        e.results = combinedResults;
-        reject(e);
-      }
-
-      if (combinedResults.errors.length === 0) {
-        delete combinedResults.errors;
-      }
-  
-      return resolve(combinedResults);
-    };
-    
-
-    // start requesters
-    for (let i = 0; i < state.max_requests; i++) {
-      state.requester_count += 1;
-      requester({ func, queue, state, combinedResults, done }); 
-    }
-  });
+function hitRateLimit(response) {
+  return 'rate-limit-remaining' in response._response.headers && parseInt(response._response.headers['rate-limit-remaining']) === 0;
 }
 
+
+module.exports = BBPromise.promisify(BatchRequest);
